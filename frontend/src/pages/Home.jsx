1fc14c7de2f8fa2e4624aeea7ac0b58fed47b6f9
Daniel Nelson <lukannelson@gmail.com>
2024-02-04 02:58:22 -0500

    added first image and first custom font

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
+
 export default function HomePage() {
   return (
   <div className="home-page">
-  <img src="your-image-url.jpg" alt="Description" className="left-image" />
+  <img src="images/IMG_0747.JPG" alt="Description" className="left-image" />
   <div className="content">
-    <h1>SoloSphere</h1>
-    <p>Put something interesting here!</p>
+    <h1><span>Solo</span>Sphere</h1>
+    <p>Discover Art Beyond Boundaries: Welcome to SoloSphere</p>
   </div>
 </div>
   )
+  
 }
